459612109a4cb81635d56bb5b3989d260465a001
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2025-01-07 16:10:47 +0100

    add sub-project "example"

---
+++ projects/examples/main.ts
@@ -0,0 +1,10 @@
+// @ts-ignore <ts(2307)> (lokalny moduł)
+import { add, subtract } from "@j-Cis/mod-deno-math";
+
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+// --module esnext
+if (import.meta.main) {
+  console.log("1 + 2 =", add(1, 2));
+  console.log("2 - 4 =", subtract(2, 4));
+}
